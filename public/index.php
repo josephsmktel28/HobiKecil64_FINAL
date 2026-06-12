7f2181eb3bb01fcb29b35e832d0a5b1c0c2011cb
josephsmktel28 <joseph.satrio705@gmail.com>
2026-06-12 12:37:22 +0700

    Force HTTPS natively at index.php to resolve insecure form actions

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Paksa request untuk dideteksi sebagai HTTPS ketika berada di lingkungan proxy Render
+if ((isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') || getenv('RENDER') || isset($_ENV['RENDER'])) {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
